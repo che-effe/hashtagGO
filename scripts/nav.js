857d6be7586a135c0268ed11e639feb562dd37ef
CF Grugan <charles_grugan@cable.comcast.com>
2015-02-18 19:20:02 -0500

    Making Hamburgers and Fixing main button breakpoint

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,8 +1,8 @@
 (function(){
-  var navIcon = document.getElementById('navIcon');
+  var hamburger = document.getElementById('navIcon');
   var menu = document.getElementById('menu');
 
-  navIcon.addEventListener("click", function(){
+  hamburger.addEventListener("click", function(){
     if(menu.classList.contains('shown')) {
       menu.classList.remove('shown')
     } else {
